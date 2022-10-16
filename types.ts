19c6018ba76f64055cdc54f109c9d6d6121e8ead
neil <neil@neils-MacBook-Pro.local>
2022-10-16 09:45:47 +0800

    add data types

---
+++ types.ts
@@ -0,0 +1,33 @@
+// collection: /teams/:team_id
+export interface Team {
+    name: string;
+    slug: string;
+    created_at?: Date;
+}
+
+export enum Days {
+    SUNDAY = 'SUNDAY',
+    MONDAY = 'MONDAY',
+    TUESDAY = 'TUESDAY',
+    WEDNESDAY = 'WEDNESDAY',
+    THURSDAY = 'THURSDAY',
+    FRIDAY = 'FRIDAY',
+    SATURDAY = 'SATURDAY'
+}
+
+// collection: /teams/:team_id/users/:user_id
+export interface User {
+    email: string;
+    tz: string;
+    tz_offset: number; // offset from UTC+0 +8, -3
+}
+
+// collection: /teams/:team_id/availability/:availability_id
+export interface Availability {
+    user_email: string;
+    day: Days;
+    start_at: number; // 0-23.5 ie: 12:30AM:0.5,  12NN:12 12MN:0
+    end_at: number; // 0.5-24 ie: 12MN:24 NOTE: diff of 12MN between start vs end
+    tz_offset: number;
+    tz: string;
+}
